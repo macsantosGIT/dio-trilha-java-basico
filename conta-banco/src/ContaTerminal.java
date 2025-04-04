58ec3e2cd56e32bfdb0cd9d7a2d1142258708c39
macsantosGIT <marcelo.correa.santos@gmail.com>
2025-04-04 18:28:29 -0300

    Finalizando projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Eu nao inclui o Locale na classe Scaner, porque na minha maquina nao estah formatando corretamento
+        //estah dando erro de java.util.InputMismatchException
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da agência com digito(se houver)! Ex.: 9999-9. (depois ENTER para o próximo campo)");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite o numero da conta sem dígito! Ex.: 9999. (depois ENTER para o próximo campo)");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite seu nome completo! (depois ENTER para o próximo campo)");
+        String nome = scanner.next();
+        System.out.println("Por favor, digite o saldo! Ex.: 9999,99 (depois ENTER para o próximo campo)");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numeroConta +" e seu saldo "+ saldo +" já está disponível para saque!");
     }
 }
